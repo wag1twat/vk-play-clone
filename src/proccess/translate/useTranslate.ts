74d55296a009d123672c20497f5f9ced28b6cd46
wag1twat <up.pavlogradsky@gmail.com>
2023-03-07 05:15:17 +0300

    desc/use-translate

--- src/proccess/translate/useTranslate.ts
+++ src/proccess/translate/useTranslate.ts
@@ -16,7 +16,7 @@ const loadModule = async (entity: Entity, lang: Lang) => {
  * @returns функция перевода
  */
 // TODO: оверкилл ли гард на загружаемый модуль ??? норм для дебага перевести только на proccess.env = dev
-// TODO: че делать с миганием текста на замене placeholder, мб skeleton / react.suspense + useTransition
+// TODO: че делать с миганием текста на замене placeholder при перезагрузке страницы, мб skeleton / react.suspense + useTransition
 export const useTranslate = (entity: Entity, lang: Lang) => {
   const entityRef = React.useRef<Entity>(entity)
   const langRef = React.useRef<Lang>(lang)
