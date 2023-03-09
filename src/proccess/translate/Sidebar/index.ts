3a3689e7a71509f61e9fc7baa2447dd6cc45561f
wag1twat <up.pavlogradsky@gmail.com>
2023-03-09 16:24:41 +0300

    feat/copy-click-board-id

--- src/proccess/translate/Sidebar/index.ts
+++ src/proccess/translate/Sidebar/index.ts
@@ -8,6 +8,10 @@ export const sidebarTranslate = Record({
     support: String,
     lang: String,
     exit: String,
+    "clickboard-id": Record({
+        "aria-label": String,
+        "toast-message": String
+      })
   }),
 })
 
